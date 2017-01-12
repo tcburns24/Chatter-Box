0a20abec25bcf82d76e2c02179fba464bf423e9c
Thomas Burns <tcburns24@gmail.com>
2017-01-12 14:09:28 -0800

    ng-repeat Revisions

--- app/scripts/controllers/RoomsCtrl.js
+++ app/scripts/controllers/RoomsCtrl.js
@@ -2,7 +2,7 @@
     function RoomsCtrl(Room){
         // make list of rooms available to the view by attaching them to the controller
         // ex: this.rooms
-        this.rooms = Room;
+        this.rooms = Room.all;
     }
     
     angular
