f2d3358a4baaeb5c8aa2df2e120b5dbca42d1924
Thomas Burns <tcburns24@gmail.com>
2017-04-19 12:27:19 -0700

    4.19.2017 changes

--- dist/scripts/controllers/RoomsCtrl.js
+++ dist/scripts/controllers/RoomsCtrl.js
@@ -3,7 +3,7 @@
         // make list of rooms available to the view by attaching them to the controller
         // ex: this.rooms
 		this.room = Room;
-        this.rooms = Room.all;
+        this.rooms = Room.all; 
 		this.message = Message;
 		this.messages = null;
 		this.currentRoom = null;
@@ -12,13 +12,13 @@
 			this.currentRoom = room;
 		}
 		this.cookie = BlocChatCookies;
-		
+
 		// user clicks room name
 		// run selectRoom and pass in room id
 		// use the room id to filter the messages
 		// set the messages property to the filtered message array
     }
-    
+
     angular
         .module('blocChat')
         .controller('RoomsCtrl', ['Room', 'Message', 'BlocChatCookies', RoomsCtrl])
